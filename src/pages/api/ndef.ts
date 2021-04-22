38507c42561702abfc857ca726d032453ed1d441
Dhika Rizky <deka_r@outlook.com>
2021-04-22 13:09:11 +0700

    Added NDEF event logging in fauna

--- src/pages/api/ndef.ts
+++ src/pages/api/ndef.ts
@@ -1,6 +1,22 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import fauna from 'faunadb'
+
+const q = fauna.query
+const client = new fauna.Client({
+  secret: process.env.FAUNA_SECRET || ''
+})
 
 export default async function (req: NextApiRequest, res: NextApiResponse): Promise<void> {
-  console.log(req.body)
+  await client.query(
+    q.Create(
+      q.Collection('logs'),
+      {
+        data: {
+          ndefSerialNumber: req.body.ndefId,
+          createdAt: new Date().getTime()
+        }
+      }
+    )
+  )
   res.status(200).json({ message: 'ok' })
 }
